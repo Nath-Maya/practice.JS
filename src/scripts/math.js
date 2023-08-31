195f3d5763a4b562a76a827c6a9f2dbc38900330
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-08-30 23:40:01 -0500

    Formulas calculo triangulo y cuadrado

--- src/scripts/math.js
+++ src/scripts/math.js
@@ -1,1 +1,18 @@
+const ladoCuadrado = 5;
 
+const perimetroCuadrado = ladoCuadrado * 4;
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log({ ladoCuadrado, perimetroCuadrado, areaCuadrado });
+
+const ladoTriangulo = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo + ladoTriangulo2 + baseTriangulo;
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+console.log({ladoTriangulo, ladoTriangulo2, baseTriangulo, alturaTriangulo, perimetroTriangulo, areaTriangulo})
